fd61df4792c1111cb2f658e05957874d6b80ef40
udhayasudharsanan <udhayasudharsanank@gmail.com>
2025-07-04 21:40:12 +0530

    Add Week 3

---
+++ week3/OrmLearnSpringDataJPA/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,16 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.*;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+
+@Service
+public class CountryService {
+    @Autowired private CountryRepository countryRepository;
+    @Transactional public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
